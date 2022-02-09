dc5927d8dfbbb1b7b51ac641bc55966b2fe12024
dhruviben <dh342773@dal.ca>
2022-02-09 15:36:56 -0400

    my portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,117 @@
+let resumeData = {
+    "imagebaseurl":"https://www.linkedin.com/in/dhruvi-shah-aa47a71b4/",
+    "name": "Dhruvi Shah",
+    "role": "Student",
+    "linkedinId":"Dhruvi Shah",
+    "EmailID": "dhruvishah1817@gmail.com",
+    "skypeid": "Your skypeid",
+    "roleDescription": " and i want to persue my carrer as Full stack Developer and i am very passionate individual and very hard working women. I like to learn new things and explore adventurous places.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/dhruvi-shah-aa47a71b4/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/dhshah30",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"mailto:dhruvishah1817@gmail.com",
+          "className":"fa fa-email"
+        },
+        {
+          "name":"Email",
+          "url":"mailto:dhruvishah1817@gmail.com",
+          "className":"fa fa-email"
+        }
+
+      ],
+    "aboutme":"I am currently persuing Masters at Dalhousie University. I want to persue my career as Full stack Developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them and never give up.",
+    "address":"India",
+    "website":"https://github.com/dhshah30",
+    "education":[
+      {
+        "UniversityName":"Dalhousie University",
+        "specialization":"Computer Science",
+        "YearOfPassing":"Expected Sept 2022",
+        "Learning":"Software development technologies and web development"
+      },
+      {
+        "UniversityName":"SilverOak University",
+        "specialization":"Bachelor of Computer Engineering",
+        "YearOfPassing":"Sept 2020",
+        "Learning":"C, C++, Java, Advanced Java, JavaScript, PHP, HTML5, Python"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Amkay Software Solution",
+        "specialization":"Software Developer and Web Developer",
+        "YearOfLeaving":"November 2020",
+        "Achievements":"Created multiple websites using JavaScript, HTML5,CSS3, bootstrap and PHP as a web developer where I understand the requirements of the clients and fulfilled them on time. Worked as python developer for the company where created software for data entry using ML and python. Engaged with clients on every milestone and tried to meet up with their expectation from the project. Technologies: JavaScript, HTML5,CSS3, PHP, Bootstrap, Word press, Python, Machine Learning."
+         },
+      {
+        "CompanyName":"DesignX",
+        "specialization":"Java Developer",
+        "YearOfLeaving":"September 2019",
+        "Achievements":"Worked as java developer for the company where worked on making software about restaurants and online food ordering. Worked in different projects as a beginner. Learned to work in a professional environment. Gathered client’s requirement to improve the software.        "
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":" HTML5"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Javascript"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"University Management System ",
+        "Subject":"Advanced Web Development",
+        "description":"Designed web application of university management using react components. Students can get information about different universities and their available courses. Students can also get access to library facility online also can book for their residency services.",
+        "imgurl":"images/portfolio/download.jpg",
+        "Project Technologies": "Technologies: React.js and Node.js, jQuery, JavaScript, JSON, XML, HTML, Bootstrap "
+      },
+      {
+        "name":"Database Management ",
+        "Subject":"Database Warehousing",
+        "description":"Designed full distributed database system which is able to parse actual SQL queries Query Processing and execution User Authentication and Authorization",
+        "imgurl":"images/portfolio/project.jpg",
+        "Project Technologies": "Data Structure and Java, MySQL, SQL Workbench"
+      },
+      {
+        "name":"Blood Donation",
+        "subject":"Advance Software Development Concepts",  
+        "description":"Created software for blood donation booking using java technology. Users can get information about donating blood and details about locations where blood is available. Users can also get certificate of blood donation and vaccination slots are also available.",
+        "imgurl":"images/portfolio/project2.png",
+        "Project Technologies": "Java"
+      },
+      {
+        "name":"Programming Hub",
+        "subject": "Bachelor of Computer Engineering",
+        "description":"It was my final year’s project in my bachelors of engineering where we created an app for the students to get all the required materials online and to guide them through their academic career. There was admin and user side. In user side user can log in and search for their required material. More benefits about this app are when user will get notification every time any new material related to their search is available.",
+        "imgurl":"images/portfolio/phone.jpg",
+        "Project Technologies": "Java and Android"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"She has done some amazing work while doing internshi at Amkay Software Solution. Her ability to communicate with client and fulfill their requirment on time was amazing thing about her work apart from her technical abilities.",
+        "name":"Masoomi kataria, CEO of Amkay Software Solution"
+      },
+      {
+        "description":"One of the very hard-working and determined individual that i have worked with. always up for learning new concepts.",
+        "name":"Deep Sheth, Employer at DesignX"
+      }
+    ]
+  }
+  
+  export default resumeData
